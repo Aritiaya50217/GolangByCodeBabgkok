a83f9605c2271f9300cb6117b20cd3bfe933ddaf
artitaya <artitaya2466@gmail.com>
2025-07-30 17:44:14 +0700

    no message

--- fiber/main.go
+++ fiber/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gofiber/fiber/v2"
+	jwtware "github.com/gofiber/jwt/v2"
+	"github.com/jmoiron/sqlx"
+)
+
+var db *sqlx.DB
+
+const jwtSecret = "infinitas"
 
 func main() {
-	fmt.Println("")
+	var err error
+	db, err = sqlx.Open("mysql", "root:test@tcp(localhost:3306)/bank")
+	if err != nil {
+		panic(err)
+	}
+	app := fiber.New()
+	app.Use("/hello", jwtware.New(jwtware.Config{
+		SigningMethod: "HS256",
+		SigningKey:    []byte(jwtSecret),
+		SuccessHandler: func(c *fiber.Ctx) error {
+			return c.Next()
+		},
+		ErrorHandler: func(c *fiber.Ctx, e error) error {
+			return fiber.ErrUnauthorized
+		},
+	}))
+	app.Get("/hello", Hello)
+
+	app.Listen(":8000")
+}
+
+func Hello(c *fiber.Ctx) error {
+	return c.SendString("Hello World")
 }
